e127318532d0990a41b88ca07acd6477bed2442d
rnicolae <rnicolae@deloittece.com>
2025-04-05 19:44:25 +0300

    feature/metrics-and-readme-update: Added metrics for prometheus, updated readme, added postman collection used to test

--- src/test/java/local/dev/storemanager/application/kafka/ProductEventPublisherTest.java
+++ src/test/java/local/dev/storemanager/application/kafka/ProductEventPublisherTest.java
@@ -1,21 +1,27 @@
 package local.dev.storemanager.application.kafka;
 
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import local.dev.storemanager.application.event.ProductEvent;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import static local.dev.storemanager.config.KafkaTopics.PRODUCTS_TOPIC;
+import static local.dev.storemanager.config.Metrics.PRODUCT_EVENTS_PUBLISHED_TOTAL;
+import static local.dev.storemanager.constants.EventTypes.PRICE_CHANGED;
 import static local.dev.storemanager.constants.EventTypes.PRODUCT_CREATED;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class ProductEventPublisherTest {
 
+    private final KafkaTemplate<String, ProductEvent> kafkaTemplate = mock();
+    private final SimpleMeterRegistry meterRegistry = new SimpleMeterRegistry();
+
+    private final ProductEventPublisher publisher = new ProductEventPublisher(kafkaTemplate, meterRegistry);
+
     @Test
     void shouldPublishProductEventToKafka() {
-        final var kafkaTemplate = mock(KafkaTemplate.class);
-        final var publisher = new ProductEventPublisher(kafkaTemplate);
 
         final var event = new ProductEvent(PRODUCT_CREATED, "Product payload");
 
@@ -28,4 +34,22 @@ class ProductEventPublisherTest {
         assertEquals(PRODUCT_CREATED, capturedEvent.getType());
         assertEquals("Product payload", capturedEvent.getPayload());
     }
+
+    @Test
+    void shouldPublishEventAndIncrementCounter() {
+        final var event = new ProductEvent("1", PRICE_CHANGED);
+
+        publisher.publish(PRODUCT_CREATED, event);
+
+        verify(kafkaTemplate).send(PRODUCT_CREATED, event);
+
+        final var counter = meterRegistry
+                .get(PRODUCT_EVENTS_PUBLISHED_TOTAL)
+                .tag("topic", PRODUCT_CREATED)
+                .tag("eventType", event.getType())
+                .counter();
+
+        assertEquals(1.0, counter.count(), 0.01);
+    }
+
 }
